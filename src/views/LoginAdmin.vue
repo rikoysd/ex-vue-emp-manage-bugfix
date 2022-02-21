e631a8ba14c00a26f57cd51a7c534bdb49d0443b
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-21 15:18:26 +0900

    ログイン失敗時のエラー処理完了

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div class="row login-page">
+      <div>{{ errorMessage }}</div>
       <div class="col s12 z-depth-6 card-panel">
         <form class="login-form">
           <div class="row"></div>
@@ -65,6 +66,8 @@ export default class LoginAdmin extends Vue {
   private mailAddress = "";
   // パスワード
   private password = "";
+  // エラーメッセージ
+  private errorMessage = "";
 
   /**
    * ログインする.
@@ -80,8 +83,12 @@ export default class LoginAdmin extends Vue {
     });
     console.dir("response:" + JSON.stringify(response));
 
-    // 従業員一覧に遷移する
-    this.$router.push("/employeeList");
+    if (response.data.status === "success") {
+      // 従業員一覧に遷移する
+      this.$router.push("/employeeList");
+    } else {
+      this.errorMessage = "ログインに失敗しました";
+    }
   }
 }
 </script>
